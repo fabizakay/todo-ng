b4d99ffa2a053ba2e293473b61705d1b1a65155c
fabi.zakay <fabizakay1212@gmail.com>
2022-03-24 19:12:53 +0200

    Add sort + search + some improve in update

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -4,6 +4,13 @@ import { NgForm } from '@angular/forms';
 export interface Task{
   name:string;
   isUpdated:boolean;
+  isVisible:boolean;
+}
+enum sortOption{
+  ASC = 'asc',
+  DESC = 'desc',
+  NONE = 'none'
+
 }
 
 @Component({
@@ -14,6 +21,8 @@ export interface Task{
 export class TodoComponent implements OnInit {
 
   tasks:Task[]= [];
+  SortEnum = sortOption;
+  sort:sortOption=sortOption.NONE;
 
   constructor() { 
     
@@ -23,7 +32,7 @@ export class TodoComponent implements OnInit {
   }
 
   handleSubmit(addForm:NgForm){
-    let newTask = {name:addForm.value.task, isUpdated:false};
+    let newTask = {name:addForm.value.task, isUpdated:false, isVisible:false};
     this.tasks.push(newTask);
     addForm.resetForm();
   }
@@ -37,6 +46,52 @@ export class TodoComponent implements OnInit {
     let updatedTask:Task = this.tasks.filter((t)=> t.name === oldName)[0]
     updatedTask.name=newTaskname;
     updatedTask.isUpdated = false;
+  }
+  handleSort(sortDirection:sortOption){
+    
+    if(sortDirection === this.sort){
+      this.sort = sortOption.NONE;
+      return;
+    }
+    this.sort = sortDirection;
+    switch (sortDirection) {
+      case sortOption.ASC:
+        this.tasks = this.tasks.sort((a,b)=> {
+          let aLower = a.name.toLowerCase();
+          let bLower = b.name.toLowerCase();
+          if(aLower<bLower){
+            return -1;
+          }
+          if(aLower> bLower){
+            return 1
+          }
+          return 0;
+        });
+        break;
+        case  sortOption.DESC:
+          this.tasks = this.tasks.sort((a,b)=> {
+            let aLower = a.name.toLowerCase();
+            let bLower = b.name.toLowerCase();
+            if(aLower<bLower){
+              return 1;
+            }
+            if(aLower> bLower){
+              return -1
+            }
+            return 0;
+          });
+          break;
+          case sortOption.NONE:
+      default:
+        break;
+    }
+
+    
+  }
+  handleSearch(v:string){
+      this.tasks.map((task)=>{
+        task.isVisible=(task.name.includes(v));
+      });
   }
 
 }
